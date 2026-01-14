5ffe95d39abe38e67fea61066e23a1fb7c126417
kundai <chindondik@praz.org.zw>
2026-01-14 10:16:53 +0200

    mne add to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('surname');
-            $table->string('phone');
+            $table->string('surname')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level')->nullable();
+            $table->string('status')->nullable();
+            $table->bigInteger('schoolnumber_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
